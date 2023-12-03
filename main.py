3860692d2404d8daf13191819620513d5a29e800
Emre Duman <76225090+Emre1Duman@users.noreply.github.com>
2023-12-03 15:57:51 +0000

    Check if it will rain every 12hrs

--- main.py
+++ main.py
@@ -9,12 +9,24 @@ parameters = {
     "lat": LAT,
     "lon": LON,
     "appid": API_KEY,
+    "cnt": 4,
 }
 
 respone = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", params=parameters)
 respone.raise_for_status()
 print(respone)
-data = respone.json()
-print(data)
+weather_data = respone.json()
+
+#Loop through next 12 hours
+will_rain = False
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    print("Bring an umbrella.")
+
+
 
 
